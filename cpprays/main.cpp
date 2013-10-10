60de6b42e7274b67fe0fa45b401e18c3d23212e5
Karan Misra <kidoman@gmail.com>
2013-10-10 12:59:18 +0530

    c++: bring benchmark semantics in line with ref Go version

--- cpprays/main.cpp
+++ cpprays/main.cpp
@@ -316,8 +316,6 @@ int main(int argc, char **argv) {
   const auto objects = makeObjects(art);
   Result result(cl.times);
 
-  const auto overallDurationBegin = Clock::now();
-
   const auto imageSize = static_cast<int>(sqrt(cl.megaPixels * 1000.0 * 1000.0));
   std::vector<unsigned char> bytes(3 * imageSize * imageSize);
   const auto clamp = [](float v) -> unsigned char {
@@ -328,15 +326,8 @@ int main(int argc, char **argv) {
     }
   };
 
-  const auto g = !vector(-3.1f, -16.f, 1.9f);
-  const auto a = !(vector(0.0f, 0.0f, 1.0f)^g) * .002f;
-  const auto b = !(g^a)*.002f;
-  const auto c = (a+b)*-256.0f+g;
-  const auto ar = 512.0f / static_cast<float>(imageSize);
-  const auto orig0 = vector(-5.0f, 16.0f, 8.0f);
-
-  auto lambda=[&](unsigned int seed, int offset, int jump) {
-    for (int y=offset; y<imageSize; y+=jump) {    //For each row
+  auto lambda = [&](vector a, vector b, vector c, float ar, vector orig0, unsigned int seed, int offset, int jump) {
+    for (int y=offset; y<imageSize; y+=jump) {
       int k = (imageSize - y - 1) * imageSize * 3;
 
       for(int x=imageSize;x--;) {
@@ -364,12 +355,22 @@ int main(int argc, char **argv) {
     }
   };
 
+  const auto overallDurationBegin = Clock::now();
+
   for(int iTimes = 0; iTimes < cl.times; ++iTimes) {
     const auto t0 = Clock::now();
+
+    const auto g = !vector(-3.1f, -16.f, 1.9f);
+    const auto a = !(vector(0.0f, 0.0f, 1.0f)^g) * .002f;
+    const auto b = !(g^a)*.002f;
+    const auto c = (a+b)*-256.0f+g;
+    const auto ar = 512.0f / static_cast<float>(imageSize);
+    const auto orig0 = vector(-5.0f, 16.0f, 8.0f);
+
     std::mt19937 rgen;
     std::vector<std::thread> threads;
     for(int i = 0; i < cl.procs; ++i) {
-      threads.emplace_back(lambda, rgen(), i, cl.procs);
+      threads.emplace_back(lambda, a, b, c, ar, orig0, rgen(), i, cl.procs);
     }
     for(auto& t : threads) {
       t.join();
